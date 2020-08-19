95a21e2523fd499c1f6455ae7facf113cee72682
feerglas <info@feerglas.com>
2020-08-19 14:57:46 +0200

    feat: add root route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Lyne Design System. Simple express server to listen to webhooks send from Figma. Figma webhook will send POST requests to /figma-change');
+})
+
 app.get('/figma-change', (req, res) => {
   res.send('Figma changed')
 })
