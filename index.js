ac06701eaf986321eb028611a19e7a22fadf68bb
feerglas <info@feerglas.com>
2020-08-19 17:11:20 +0200

    correct port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Lyne Design System. Simple express server to listen to webhooks send from Figma. Figma webhook will send POST requests to /figma-change');
@@ -10,6 +10,6 @@ app.get('/figma-change', (req, res) => {
   res.send('Figma changed')
 })
 
-app.listen(port, () => {
-  console.log(`Listening to http://localhost:${port}.`);
+app.listen(PORT, () => {
+  console.log(`Listening to http://localhost:${PORT}.`);
 })
