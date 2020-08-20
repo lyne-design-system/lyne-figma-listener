9d14bf351e7878f10bd84518eeb25f289a19d42e
feerglas <info@feerglas.com>
2020-08-20 09:25:06 +0200

    debug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-app.use(express.json());
+//app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
