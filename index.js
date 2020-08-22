340c7a0dfcc6d013b5de44e7c37b65ed7080c94a
feerglas <info@feerglas.com>
2020-08-22 10:19:44 +0200

    chore: add linter and fix style issues

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const request = require("request");
+const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-require('dotenv').config();
+require('dotenv')
+  .config();
 
 const app = express();
-//app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
@@ -15,10 +19,10 @@ const triggerTravis = () => {
   const travisUrl = 'https://api.travis-ci.org/repo/lyne-design-system%2Flyne-design-tokens/requests';
   const travisToken = process.env.TRAVIS_TOKEN;
   const headers = {
-    'Content-Type': 'application/json',
     'Accept': 'application/json',
-    'Travis-API-Version': '3',
-    'Authorization': `token ${travisToken}`
+    'Authorization': `token ${travisToken}`,
+    'Content-Type': 'application/json',
+    'Travis-API-Version': '3'
   };
 
   const body = {
@@ -28,20 +32,20 @@ const triggerTravis = () => {
   }
 
   request({
-    uri: travisUrl,
+    body: JSON.stringify(body),
+    headers,
     method: 'POST',
-    headers: headers,
-    body: JSON.stringify(body)
+    uri: travisUrl
   });
 };
 
 app.get('/', (req, res) => {
   res.send('Lyne Design System. Simple express server to listen to webhooks send from Figma. Figma webhook will send POST requests to /figma-change');
-})
+});
 
 app.post('/figma-change', (req, res) => {
-  const isCorrectFile = req.body.file_name == process.env.FIGMA_FILE_NAME;
-  const isCorrectPasscode = req.body.passcode == process.env.FIGMA_PASSCODE;
+  const isCorrectFile = req.body.file_name === process.env.FIGMA_FILE_NAME;
+  const isCorrectPasscode = req.body.passcode === process.env.FIGMA_PASSCODE;
 
   if (!isCorrectFile || !isCorrectPasscode) {
     res.sendStatus(400);
@@ -51,7 +55,7 @@ app.post('/figma-change', (req, res) => {
     // Figma needs status code 200 as answer
     res.sendStatus(200);
   }
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Listening to http://localhost:${PORT}.`);
