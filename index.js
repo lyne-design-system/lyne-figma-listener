8d24aefd5e22d8ce2c2685a21982df5715f98a6e
feerglas <info@feerglas.com>
2020-08-24 15:33:10 +0200

    fix: simplify commit message

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 
-const triggerTravis = (commitPrefix) => {
+const triggerTravis = (commitMessage) => {
   const travisUrl = 'https://api.travis-ci.org/repo/lyne-design-system%2Flyne-design-tokens/requests';
   const travisToken = process.env.TRAVIS_TOKEN;
   const headers = {
@@ -27,7 +27,7 @@ const triggerTravis = (commitPrefix) => {
 
   const body = {
     request: {
-      message: `${commitPrefix}: Triggered from Figma Library update`
+      message: `${commitMessage}: Triggered from Figma Library update`
     }
   }
 
@@ -39,21 +39,6 @@ const triggerTravis = (commitPrefix) => {
   });
 };
 
-const getCommitPrefixFromLibraryUpdateDescription = (description) => {
-  const descriptionSeparator = '**';
-  const defaultPrefix = 'chore';
-  const descriptionSplit = description.split(descriptionSeparator);
-
-  if (descriptionSplit.length !== 3) {
-    console.log('WARNING -->> commit message from library update is unclear.');
-
-    return defaultPrefix;
-  }
-
-  return descriptionSplit[1];
-
-};
-
 app.get('/', (req, res) => {
   res.send('Lyne Design System. Simple express server to listen to webhooks send from Figma. Figma webhook will send POST requests to /figma-change');
 });
@@ -61,12 +46,11 @@ app.get('/', (req, res) => {
 app.post('/figma-change', (req, res) => {
   const isCorrectFile = req.body.file_name === process.env.FIGMA_FILE_NAME;
   const isCorrectPasscode = req.body.passcode === process.env.FIGMA_PASSCODE;
-  const commitPrefix = getCommitPrefixFromLibraryUpdateDescription(req.body.description);
 
   if (!isCorrectFile || !isCorrectPasscode) {
     res.sendStatus(400);
   } else {
-    triggerTravis(commitPrefix);
+    triggerTravis(req.body.description);
 
     // Figma needs status code 200 as answer
     res.sendStatus(200);
