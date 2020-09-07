460ac7ba9dd9b129258ba7504b7088c5858f6d13
feerglas <info@feerglas.com>
2020-09-07 10:38:25 +0200

    Add log

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/figma-change', (req, res) => {
+  console.log(req.body);
   const isFileTokens = req.body.file_name === process.env.FIGMA_FILE_NAME_TOKENS;
   const isFileIcons = req.body.file_name === process.env.FIGMA_FILE_NAME_ICONS;
   const isCorrectPasscode = req.body.passcode === process.env.FIGMA_PASSCODE;
